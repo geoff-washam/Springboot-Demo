d83de0841877571f83d5c4f9ebc2a539d277b72a
gwasham <gewasham@gmailcom>
2017-02-14 23:07:09 -0800

    most important comment

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     // Adding a stupid comment... this is main!
+    // yada yada
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
